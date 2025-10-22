6b1a88c4fcb9440c3267e5653fd6bfad33472bf6
Dmytro Dorofeyev <dorofeyev74@gmail.com>
2025-10-22 21:18:49 +0200

    fix: корректная инициализация formData с пустыми значениями

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,10 +1,13 @@
 const form = document.querySelector('.feedback-form');
 const STORAGE_KEY = 'feedback-form-state';
 
-let formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+let formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {
+  email: '',
+  message: '',
+};
 
-if (formData.email) form.elements.email.value = formData.email;
-if (formData.message) form.elements.message.value = formData.message;
+form.elements.email.value = formData.email;
+form.elements.message.value = formData.message;
 
 form.addEventListener('input', evt => {
   formData[evt.target.name] = evt.target.value.trim();
@@ -17,16 +20,17 @@ form.addEventListener('submit', evt => {
   const email = form.elements.email.value.trim();
   const message = form.elements.message.value.trim();
 
+  formData = { email, message };
+
   if (email === '' || message === '') {
     alert('Fill please all fields');
     return;
   }
 
-  formData = { email, message };
-
   console.log('Sending data:', formData);
 
   localStorage.removeItem(STORAGE_KEY);
   form.reset();
-  formData = {};
+
+  formData = { email: '', message: '' };
 });
